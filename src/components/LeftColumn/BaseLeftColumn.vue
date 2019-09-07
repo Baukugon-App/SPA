e1e2a69a9ce9c91dd4f5b76a407db6265e9b6724
BaseballGear <nicholas.Yenter@cuw.edu>
2019-09-07 18:10:12 -0500

    Gutted most of the project

--- src/components/LeftColumn/BaseLeftColumn.vue
+++ src/components/LeftColumn/BaseLeftColumn.vue
@@ -1,39 +1,12 @@
 <template>
   <div class="container">
-    <v-layout column>
-      <div>
-        <List header="Show me" title="Custom Title" />
-      </div>
-
-      <div>
-        <List header="I am getting there" title="get me theree" />
-      </div>
-
-      <div>
-        <List header="Weird" title="wild" />
-      </div>
-    </v-layout>
   </div>
 </template>
 
 
 <script>
-import List from "./List";
-import Vue from "vue";
-import Vuetify from "vuetify";
-
-Vue.use(Vuetify);
-
 export default {
   components: {
-    List
   }
 };
 </script>
-
-<style scoped>
-.container {
-  margin: 0.25rem;
-  width: 12rem;
-}
-</style>
